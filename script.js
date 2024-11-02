77182f1445de4ce50fe8def1eb8462ba22b16fc9
Robert Walsh <rwalsh@excalibur-solutions.com>
2024-11-02 14:55:40 -0400

    Initialize the page with an onload event handler

--- script.js
+++ script.js
@@ -6,3 +6,19 @@
  * 
  */
 
+function initialize() {
+    let today = new Date();
+    let firstDayOfMonth = new Date();
+    firstDayOfMonth.setDate(1);
+
+    document.getElementById("from_date_field").value = formatDate(firstDayOfMonth);
+    document.getElementById("to_date_field").value = formatDate(today);
+}
+
+function formatDate(date) {
+    let result = date.toISOString();
+    console.log(result);
+
+    let t = result.indexOf("T");
+    return result.substring(0, t);
+}
